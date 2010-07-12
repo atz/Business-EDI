26502b8764c1313f10039c25e0198e97ff4c13b9
Joe Atzberger <atz@esilibrary.com>
2010-07-12 15:32:12 +0000

    Handling for the *other* set of specs we need to follow

--- lib/Business/EDI/Spec.pm
+++ lib/Business/EDI/Spec.pm
@@ -1,6 +1,6 @@
 package Business::EDI::Spec;
 
-use base qw/Business::EDI/;
+use base qw/Business::EDI/;     # inherits AUTOLOAD for $self->{_permitted} keys
 
 use strict;
 use warnings;
@@ -15,8 +15,10 @@ use File::Spec;
 our $debug = 0;
 
 our $spec_dir;       # for the whole class
+our $syntax_dir;     # for the whole class
 our $spec_map = {
     message   => {code => 'DMD', cache => {}, keys => [qw/code mandatory repeats    /]},
+segment_group => {code => 'DMD', cache => {}, keys => [qw/code mandatory repeats    /]},
     segment   => {code => 'DSD', cache => {}, keys => [qw/pos code mandatory repeats/]},
     composite => {code => 'DCD', cache => {}, keys => [qw/pos code class def        /]},
 #   codelist  => {code => 'DCL', cache => {}, keys => []},
@@ -24,10 +26,13 @@ our $spec_map = {
 };
 
 my %fields = (
-    spec_files      => undef,
     edi_flavor      => 'edifact',   # someday could be x12 or something...
+    spec_files      => undef,
     version_default => 'd08a',
     version         => undef,
+    syntax_files    => undef,
+    syntax_default => '40100',
+    syntax          => undef,
     interactive     => 0,
 );
 
@@ -44,17 +49,24 @@ sub new {
     }
     my $stuff = {_permitted => {(map {$_ => 1} keys %fields)}, %fields};
     foreach (keys %args) {
-        $_ eq 'version' and next;  # special case
+        $_ eq 'version' and next;  # special case, probably can remove
+        $_ eq 'syntax'  and next;  # special case, probably can remove
         exists ($stuff->{_permitted}->{$_}) or croak "Unrecognized argument to new: $_ => $args{$_}";
     }
     my $self = bless($stuff, $class);
     my $version = $args{version} || $self->version || $fields{version} || $self->version_default || $fields{version_default};
-    $version eq 'default' and $version = $self->version_default || $fields{version_default};
-    $debug and warn "### Setting version $version";
-    $self->set_spec_version($version) or croak "Unrecognized spec version '$version'";
+    my $syntax  = $args{syntax } || $self->syntax  || $fields{syntax } || $self->syntax_default  || $fields{ syntax_default};
+    lc($version) eq 'default' and $version = $self->version_default || $fields{version_default};
+    lc($syntax ) eq 'default' and $syntax  = $self->syntax_default  || $fields{ syntax_default};
+    $debug and warn "### Setting syntax/version $syntax/$version";
+    $self->set_syntax_version($syntax) or croak "Unrecognized spec syntax '$syntax'";
+    $self->set_spec_version( $version) or croak "Unrecognized spec version '$version'";
     $debug and print Dumper($self);
     return $self;
 }
+
+# We have to deal with two parallel kinds of CSV definitions: the spec version and the EDIFACT syntax
+# So we have pairs of methods.
 
 sub get_spec_dir {
     my $self = shift;
@@ -66,12 +78,29 @@ sub get_spec_dir {
         @dirs = File::Find::Rule->maxdepth(1)->name($_)->directory()->in(@dirs);
     }
     # we use serial Find's so we don't have to care about OS/filesystem variations.  And we only do it once, typcially.
-    $debug and print STDERR "# spec_dir() found ", scalar(@dirs), " $target dirs:\n# ", join("\n# ", @dirs), "\n";
+    $debug and print STDERR "# get_spec_dir() found ", scalar(@dirs), " $target dirs:\n# ", join("\n# ", @dirs), "\n";
     unless (@dirs) {
         warn "Could not locate specifications directory ($target) in \@INC";
         return;
     }
     return $spec_dir = $dirs[0];
+}
+sub get_syntax_dir {
+    my $self = shift;
+    $self->{syntax_dir} and return $self->{syntax_dir};
+    $syntax_dir         and return $syntax_dir;
+    my $target = 'Business/EDI/data/edifact/iso9735';    # path relative to @INC.  Don't worry about filesystem oddities (see split below)
+    my @dirs   = @INC;
+    foreach (split /\//, $target) {
+        @dirs = File::Find::Rule->maxdepth(1)->name($_)->directory()->in(@dirs);
+    }
+    # we use serial Find's so we don't have to care about OS/filesystem variations.  And we only do it once, typcially.
+    $debug and print STDERR "# get_syntax_dir() found ", scalar(@dirs), " $target dirs:\n# ", join("\n# ", @dirs), "\n";
+    unless (@dirs) {
+        warn "Could not locate specifications directory ($target) in \@INC";
+        return;
+    }
+    return $syntax_dir = $dirs[0];
 }
 
 sub set_spec_version {
@@ -85,13 +114,32 @@ sub set_spec_version {
     $self->{spec_files} = \@files;
     return $self->{version} = $code;
 }
+sub set_syntax_version {
+    my $self = shift;
+    my $code = shift or return $self->carp_error("set_syntax_version: required syntax code argument not provided");
+    my @files = $self->find_syntax_files($code);
+    unless (@files) {
+        $self->error("set_syntax_version: Unrecognized syntax code '$code' (no csv files)");
+        return;
+    }
+    $self->{syntax_files} = \@files;
+    return $self->{syntax} = $code;
+}
 
 sub find_spec_files {
     my $self = shift;
     my $code = @_ ? shift : ($self->version || $self->version_default);
-    $code or return $self->carp_error("No EDI spec revision argument to spec_files().  Nothing to look for!");
+    $code or return $self->carp_error("No EDI spec revision argument to find_spec_files().  Nothing to look for!");
     my $dir = $self->get_spec_dir or return $self->carp_error("EDI Specifications directory missing");
     $debug and warn "get_spec_dir returned '$dir'.  Looking for $dir/*.$code.csv";
+    return File::Find::Rule->maxdepth(1)->name("*.$code.csv")->file()->in($dir);
+}
+sub find_syntax_files {
+    my $self = shift;
+    my $code = @_ ? shift : ($self->syntax || $self->syntax_default);
+    $code or return $self->carp_error("No EDI spec revision argument to find_syntax_files().  Nothing to look for!");
+    my $dir = $self->get_syntax_dir or return $self->carp_error("EDI Syntax directory missing");
+    $debug and warn "get_syntax_dir returned '$dir'.  Looking for $dir/*.$code.csv";
     return File::Find::Rule->maxdepth(1)->name("*.$code.csv")->file()->in($dir);
 }
 
@@ -114,28 +162,74 @@ sub get_spec_handle {
     open (my $fh, "<$file") or carp "get_spec_handle failed to open $file";
     return $fh;
 }
+sub get_syntax_handle {
+    my $self    = shift;
+    my $type    = shift || '';
+    my $syntax  = @_ ? shift : $self->syntax;
+    $syntax or return $self->carp_error("spec syntax is not set (nor passed as a parameter)");
+    my $trio;
+    unless ($type and $trio = $spec_map->{$type}) {
+        return $self->carp_error("Type '$type' is not mapped to a syntax file.  Options are: " . join(' ', keys %$spec_map));
+    }
+    my @files = $self->find_syntax_files;
+    my $name  = $self->csv_filename('S', $trio->{code}, $syntax);
+    $debug and print STDERR "get_syntax_handle() checking " . scalar(@files) . " files for: $name\n";
+    my @hits = grep {(File::Spec->splitpath($_))[2] eq $name} @files;
+    scalar(@hits) or return $self->carp_error("Syntax file for $type ($name) not found");
+    my $file = $hits[0];
+    $debug and warn "get_syntax_handle opening $file";
+    open (my $fh, "<$file") or carp "get_syntax_handle failed to open $file";
+    return $fh;
+}
 
 sub csv_filename {
     my $self = shift;
-    return ($self->interactive ? 'I' : 'E') . (shift || '') . '.' . (shift || '') . ".csv";
+    return (scalar(@_) > 2 ? shift : $self->interactive ? 'I' : 'E')
+        . (shift || '') . '.' . (shift || '') . ".csv";
 }
 
+# gets a page of the already declared spec, like say the one defining message(s)
 sub get_spec {
     my $self = shift;
     my $type = shift   or return $self->carp_error("get_spec: required argument for spec 'type' missing.  Options are: " . join(', ', keys %$spec_map));
     $spec_map->{$type} or return $self->carp_error("Type '$type' is not mapped to a spec file.  Options are: " . join(' ', keys %$spec_map));
     my $subpart = @_ ? shift : '';
-    my $version = $self->version;
-    $version or return $self->carp_error("spec version is not set (nor passed as a parameter)");
+    my $version = $self->version or return $self->carp_error("spec version is not set");
+    my $syntax  = $self->syntax  or return $self->carp_error("spec syntax is not set");
     if ($spec_map->{$type}->{cache}->{$version}) {
         $debug and print STDERR "cache hit for spec_map->{$type}->{cache}->{$version}\n";
+    } elsif ($type eq 'segment_group') {
+        my $message_spec = $self->get_spec('message', $subpart);  # segment groups are defined in the message file
+        foreach (keys %$message_spec) {
+            /^(\S+)\/(SG\d+)$/ or next; #  like ORDRSP/SG27
+            $spec_map->{$type}->{cache}->{$version}->{$1}->{$2} = $message_spec->{$_};
+        }
+        my $message_syntax = $self->get_syntax('message');  # segment groups are defined in the message file
+        foreach (keys %$message_syntax) {
+            /^(\S+)\/(SG\d+)$/ or next; #  like ORDRSP/SG27
+            $spec_map->{$type}->{cache}->{$version}->{$1}->{$2} = $message_syntax->{$_};  # combine syntax/spec defs
+        }
     } else {
         my $fh = $self->get_spec_handle($type) or return;
         $spec_map->{$type}->{cache}->{$version} = $self->parse_plexer($type, $fh);
+        my $extras = $self->get_syntax($type);
+        foreach (keys %$extras) {
+            $spec_map->{$type}->{cache}->{$version}->{$_} = $extras->{$_};                # combine syntax/spec defs
+        }
     }
     return $spec_map->{$type}->{cache}->{$version};
 }
 
+sub get_syntax {    # no (separate from spec) cache
+    my $self = shift;
+    my $type = shift   or return $self->carp_error("get_syntax: required argument for syntax 'type' missing.  Options are: " . join(', ', keys %$spec_map));
+    $spec_map->{$type} or return $self->carp_error("Type '$type' is not mapped to a spec file.  Options are: " . join(' ', keys %$spec_map));
+    $type = 'message' if ($type eq 'segment_group');    # sort out the messages vs. segments yourself
+    my $fh = $self->get_syntax_handle($type) or return;
+    return $self->parse_plexer($type, $fh);
+}
+
+# returns pseudohash
 sub parse_plexer {
     my $self = shift;
     my $type = shift or croak("parse_plexer: required argument for spec 'type' missing.  Options are: " . join(', ', keys %$spec_map));
@@ -157,16 +251,19 @@ sub parse_plexer {
                 }
             } @slurp
        };
-    } elsif ($type eq 'composite' or $type eq 'message' or $type eq 'segment') {
+    } elsif ($type eq 'composite' or $type eq 'message' or $type eq 'segment' or $type eq 'segment_group') {
         return {
             map {
                 my ($code, $label, @rest) = split ';', $_;
                 my @codeparts = split ':', $code;
-                $code = $codeparts[0];
-                $code .= "_" . $codeparts[-1] if ($codeparts[-1] and $codeparts[-1] ne $code);  # key may include _SegmentGroup
-                $debug and $debug > 1 and print STDERR "parsing CSV for $type/$code ($label)\n";
-                $code => {
-                    code    => $code,
+                my $xpath = $code = $codeparts[0];
+                if ($codeparts[-1] and $codeparts[-1] ne $code and $codeparts[-1] ne 'UN') {
+                    $xpath .= "/" . $codeparts[-1];
+                }
+                $debug and $debug > 1 and print STDERR "parsing CSV for $type/$xpath ($label)\n";
+                $xpath => {
+                    xpath   => $xpath,      # ORDERS/SG02 -- xpath is same as code except for segment_groups
+                    code    => $code,       # SG02
                 #   version => $codeparts[1] . $codeparts[2],
                     label   => $label,
                     parts   => $self->parse_array(\@rest, $spec_map->{$type}->{keys})
@@ -189,13 +286,14 @@ sub parse_array {
     (scalar(@parts) % scalar(@keys)) and croak sprintf "Cannot parse %s elements evenly into parts of %s for body: %s", scalar(@parts), scalar(@keys), join(';', @parts); 
 
     my @return;
+    my $i = 0;
     while (@parts) {
-        my %chunk;
+        my %chunk = (index => $i++);
         foreach (@keys) {
             my $value = shift @parts;
             if ($_ eq 'mandatory') {
                 next unless $value eq 'M';  # conditional is assumed
-                $value = 1;
+                $value = 1;                 # M => 1
             }
             $chunk{$_} = $value;
         }
